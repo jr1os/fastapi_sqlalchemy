a70208a2a58ad4df16aba6d696a85f82057221a9
Ali Rios <ali.rios@gmail.com>
2021-11-13 20:11:16 -0300

    :building_construction: created http methods close #2

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ from fastapi import FastAPI
 from routes.user import user
 
 
-app = FastAPI()
+app = FastAPI(
+        title="Project Fastapi",
+        description="Ali Rios",
+        version="0.0.1",
+        openapi_tags=[{
+            "name": "users",
+            "description": "users routes"}
+            ]
+        )
 app.include_router(user)
--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,54 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+from config.db import conn
+from models.user import users
+from typing import List
+from schemas.user import User
+from cryptography.fernet import Fernet
+from starlette.status import HTTP_204_NO_CONTENT
 
+key = Fernet.generate_key()
+k = Fernet(key)
 user = APIRouter()
 
 
-@user.get("/users")
-def helloworld():
-    return "hello world"
+@user.get(
+    "/users",
+    tags=["users"],
+    response_model=List[User],
+    description="Get a list of all users",
+)
+def get_users():
+    return conn.execute(users.select()).fetchall()
+
+
+@user.post("/users", tags=["users"], response_model=User)
+def create_user(user: User):
+    new_user = {"name": user.name, "email": user.email}
+    new_user["password"] = k.encrypt(user.password.encode("utf-8"))
+    result = conn.execute(users.insert().values(new_user))
+    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+
+
+@user.get("/users/{id}", tags=["users"], response_model=User)
+def get_user(id: str):
+    return conn.execute(users.select().where(users.c.id == id)).first()
+
+
+@user.delete("/users/{id}", tags=["users"], status_code=HTTP_204_NO_CONTENT)
+def delete_user(id: str):
+    conn.execute(users.delete().where(users.c.id == id))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@user.put("/users/{id}", tags=["users"], response_model=User)
+def update_user(id: str, user: User):
+    conn.execute(
+        users.update()
+        .values(
+            name=user.name,
+            email=user.email,
+            password=k.encrypt(user.password.encode("utf-8")),
+        )
+        .where(users.c.id == id)
+    )
+    return conn.execute(users.select().where(users.c.id == id)).first()
